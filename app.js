aa37d3242f394c515b69c067b7a4d5dd53ac30b5
Genevieve <vievenavales29@gmail.com>
2021-09-05 18:33:46 +0800

    input validation of all fields and user choice of tip value

--- app.js
+++ app.js
@@ -3,11 +3,15 @@
 const inputField = document.querySelector(".input__field");
 const peopleField = document.querySelector(".people__field");
 const tips = document.querySelectorAll(".tips__option");
+const customTip = document.querySelector(".tips__custom");
 
 const tipAmount = document.querySelector("#tip-amount");
 const totalAmount = document.querySelector("#total-amount");
 
 const resetBtn = document.querySelector("#reset-button");
+const billError = document.querySelector(".error-msg--1");
+const peopleError = document.querySelector(".error-msg--2");
+const tipError = document.querySelector(".error-msg--3");
 
 let value,
   bill,
@@ -17,12 +21,16 @@ let value,
 ////////////////////////////////////////////////////////////
 // BILL VALUE INPUT
 inputField.addEventListener("click", function () {
+  billError.style.display = "none";
+
   inputField.addEventListener("mouseleave", function () {
     if (inputField.value.trim() === "" || inputField.value.trim() === "0") {
       inputField.classList.add("focus-error");
+      billError.style.display = "block";
       check[0] = 0;
     } else {
       inputField.classList.remove("focus-error");
+      billError.style.display = "none";
       bill = inputField.value;
       check[0] = 1;
       computeResults();
@@ -35,26 +43,48 @@ inputField.addEventListener("click", function () {
 
 tips.forEach((tip) => {
   tip.addEventListener("click", function () {
+    tipError.style.display = "none";
+    tip.classList.remove("focus-error");
+
     if (tip.classList.contains("tips__custom")) {
       removeOtherActives(tips);
 
       tip.addEventListener("keyup", function (e) {
         e.preventDefault();
         if (e.keyCode === 13) {
-          tip.blur();
-          assignTipValue(tip.value);
-          check[1] = 1;
-          computeResults();
+          if (getCustomTip(tip.value)) {
+            tipError.style.display = "hidden";
+            tip.classList.remove("focus-error");
+            tip.blur();
+            assignTipValue(tip.value);
+            check[1] = 1;
+            computeResults();
+          } else {
+            check[1] = 0;
+            tipError.style.display = "block";
+            tip.classList.add("focus-error");
+          }
         }
       });
 
       tip.addEventListener("mouseleave", function () {
-        assignTipValue(tip.value);
-        check[1] = 1;
-        computeResults();
+        if (getCustomTip(tip.value)) {
+          tipError.style.display = "hidden";
+          tip.classList.remove("focus-error");
+          assignTipValue(tip.value);
+          check[1] = 1;
+          computeResults();
+        } else {
+          tipError.style.display = "block";
+          tip.classList.add("focus-error");
+          check[1] = 0;
+        }
       });
     } else {
       document.querySelector(".tips__custom").value = "";
+      tipError.style.display = "hidden";
+      customTip.classList.remove("focus-error");
+
       removeOtherActives(tips);
       tip.classList.add("tip-pick");
       assignTipValue(tip.innerText.slice(0, -1));
@@ -67,12 +97,18 @@ tips.forEach((tip) => {
 ////////////////////////////////////////////////////////////
 // BILL VALUE INPUT
 peopleField.addEventListener("click", function () {
+  peopleError.style.display = "none";
+
   peopleField.addEventListener("mouseleave", function () {
     if (peopleField.value.trim() === "" || peopleField.value.trim() === "0") {
       peopleField.classList.add("focus-error");
+      peopleError.style.display = "block";
+
       check[2] = 0;
     } else {
       peopleField.classList.remove("focus-error");
+      peopleError.style.display = "none";
+
       numOfPeople = peopleField.value;
       check[2] = 1;
       computeResults();
@@ -122,3 +158,13 @@ resetBtn.addEventListener("click", function () {
   removeOtherActives(tips);
   check = [0, 0, 0];
 });
+
+////////////////////////////////////////////////////////////
+// CHECK CUSTOM TIP
+
+const getCustomTip = function (customInput) {
+  console.log(customInput);
+
+  if (customInput === null || customInput.trim() === "0") return false;
+  else return true;
+};
